730a37e0ac80612fe605191239f79461068dc269
MusicAlice <angelithapatico2012@gmail.com>
2025-03-10 17:40:34 -0500

    Update conexion.php

--- ProyProgram/conexion.php
+++ ProyProgram/conexion.php
@@ -1,5 +1,12 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "Colombia1*", "pruejem");
+
+$host = "dpg-cv7mek3tq21c73cfa3eg-a";
+$port = "5432";
+$user = "pruejem_db_user";
+$password = "97lfWDEmuy5uFqK2Q9Ql78EvhnqUuiMP";
+$dbname = "pruejem_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
